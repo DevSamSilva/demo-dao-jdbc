c9a1e5bb98aea3c920eb3954c1ce67d5a338dff5
DevSamSilva <fcosamueldsn@gmail.com>
2026-02-04 16:23:44 -0300

    criando a entidade Vendedor

--- src/app/App.java
+++ src/app/App.java
@@ -1,10 +1,18 @@
 package app;
 
+import java.util.Date;
+
 import model.DepartamentoModel;
+import model.VendedorModel;
 
 public class App {
     public static void main(String[] args) {
         DepartamentoModel departamentoModel = new DepartamentoModel(1, "Livros");
         System.out.println(departamentoModel);
+
+        VendedorModel vendedorModel = new VendedorModel(21, "Bob", "bob@gmail.com", new Date(), 3000.00,
+                departamentoModel);
+
+        System.out.println(vendedorModel);
     }
 }
